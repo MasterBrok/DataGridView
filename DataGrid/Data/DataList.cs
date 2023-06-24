9ba2bc59905864aa3f47f6c1551ba767c06a59cd
MesterBrok <Brokdeveloper@gmail.com>
2023-06-24 23:02:23 +0330

    Add project files.

---
+++ DataGrid/Data/DataList.cs
@@ -0,0 +1,36 @@
+﻿using DataGrid.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGrid.Data
+{
+    public class DataList
+    {
+        public static List<Person> People = new List<Person>()
+            {
+            new Person() {Code = 1, Name = "John", Family = "Doe", PhoneNumber = "1234567890"},
+            new Person() {Code = 2, Name = "Jane", Family = "Doe", PhoneNumber = "0987654321"},
+            new Person() {Code = 3, Name = "Bob", Family = "Smith", PhoneNumber = "5555555555"},
+            new Person() {Code = 4, Name = "Alice", Family = "Johnson", PhoneNumber = "1111111111"},
+            new Person() {Code = 5, Name = "Mike", Family = "Williams", PhoneNumber = "2222222222"},
+            new Person() {Code = 6, Name = "Sarah", Family = "Brown", PhoneNumber = "3333333333"},
+            new Person() {Code = 7, Name = "David", Family = "Davis", PhoneNumber = "4444444444"},
+            new Person() {Code = 8, Name = "Emily", Family = "Taylor", PhoneNumber = "6666666666"},
+            new Person() {Code = 9, Name = "Tom", Family = "Wilson", PhoneNumber = "7777777777"},
+            new Person() {Code = 10, Name = "Olivia", Family = "Anderson", PhoneNumber = "8888888888"},
+            new Person() {Code = 11, Name = "Ethan", Family = "Clark", PhoneNumber = "9999999999"},
+            new Person() {Code = 12, Name = "Ava", Family = "White", PhoneNumber = "0000000000"},
+            new Person() {Code = 13, Name = "Daniel", Family = "Hall", PhoneNumber = "1212121212"},
+            new Person() {Code = 14, Name = "Grace", Family = "Lewis", PhoneNumber = "2323232323"},
+            new Person() {Code = 15, Name = "Ryan", Family = "Cook", PhoneNumber = "3434343434"},
+            new Person() {Code = 16, Name = "Victoria", Family = "Parker", PhoneNumber = "4545454545"},
+            new Person() {Code = 17, Name = "Kevin", Family = "Green", PhoneNumber = "5656565656"},
+            new Person() {Code = 18, Name = "Sophia", Family = "Smith", PhoneNumber = "6767676767"},
+            new Person() {Code = 19, Name = "Joshua", Family = "Adams", PhoneNumber = "7878787878"},
+            new Person() {Code = 20, Name = "Isabella", Family = "Campbell", PhoneNumber = "8989898989"},
+            };
+    }
+}
---
+++ DataGrid/Models/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGrid.Models
+{
+    public class Person
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Person()
+        {
+            
+        }
+        public Person(int code, string name, string family, string phoneNumber)
+        {
+            Code = code;
+            Name = name;
+            Family = family;
+            PhoneNumber = phoneNumber;
+        }
+    }
+}
